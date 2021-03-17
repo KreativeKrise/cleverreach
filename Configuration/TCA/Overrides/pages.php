05a34197930c2c8f63e9c8379a1140bacec9e583
KreativeKrise <kreativekrise@gmail.com>
2021-03-17 12:50:30 +0100

    [TASK] migrate registration for pagetsconfig to Overrides/pages.php

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,9 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+/* Register PageTSconfig */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'cleverreach',
+    'Configuration/TsConfig/Page/powermail.tsconfig',
+    'EXT:cleverreach - Page TSconfig'
+);
